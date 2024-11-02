dbb839b2609aa64af7d12dcb2e6c2b067a696b13
napolegend <ya.9637@yandex.ru>
2024-11-02 22:43:23 +0300

    comments to pdf

--- litres-grabber/pdf_creator.py
+++ litres-grabber/pdf_creator.py
@@ -1,14 +1,15 @@
 import os
 import img2pdf
 
-
+# Создает сам пдф файл
 def create_pdf(image_files, file_path):
     pdf_data = img2pdf.convert(image_files)
+    # Создает пустой файл, чтобы потом туда записать
     open((file_path + ".pdf"), 'a').close()
     with open((file_path + ".pdf"), "wb") as f:
         f.write(pdf_data)
 
-
+# Ищет все файлы в директории, которые являются снимками страницы
 def get_image_list(dir_path):
     image_files = [i for i in os.listdir(dir_path) if (i.endswith(".jpeg") or i.endswith(".gif") or i.endswith(".jpg"))]
     image_files = sorted(image_files, key=lambda x: int(x.split(".")[0]))
