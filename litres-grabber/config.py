b09a2b9ef12dbeaee99eb2bbb23d52352c03af08
Легенд Bonaparte <72262848+napolegend@users.noreply.github.com>
2024-11-25 01:00:05 +0300

    Update config.py

--- litres-grabber/config.py
+++ litres-grabber/config.py
@@ -1,3 +1,5 @@
-# конфиги
+"""
+Файл содержит стандартные ссылки на страницы Литреса
+"""
 URL_BOOKS = 'https://www.litres.ru/pages/get_pdf_page/?file={2}&page={0}&rt=w{3}&ft={1}'
 URL_LOGIN = 'https://www.litres.ru/pages/login/'
